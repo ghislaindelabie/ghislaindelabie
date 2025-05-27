d4edb33652311f1d06ba7ed06ad3f502740c814a
Ghislain Delabie <ghislaindelabie@Air-de-Ghislain.lan>
2025-05-27 15:51:57 +0200

    Fix Imagemagick patch: prepend instead of alias_method

--- _plugins/imagemagick_mkdir_p_patch.rb
+++ _plugins/imagemagick_mkdir_p_patch.rb
@@ -1,28 +1,23 @@
 # _plugins/imagemagick_mkdir_p_patch.rb
-#
-# Patch pour Jekyll‐Imagemagick : 
-# utilise mkdir_p au lieu de mkdir et
-# s'assure que les répertoires de langue existent.
-
 require 'fileutils'
 
 module Jekyll
   module Imagemagick
-    class Generator
-      # On redéfinit generate pour créer tous les dossiers lang avant traitement
-      alias_method :orig_generate, :generate
-
+    module GeneratorPatch
       def generate(site)
-        # Si on a des langues (polyglot), on crée _site/<lang> avant tout
+        # Avant de laisser Imagemagick créer les variantes,
+        # on crée d’abord les dossiers de langue dans _site
         if site.respond_to?(:languages) && site.languages.any?
           site.languages.each do |lang|
-            dir = File.join(site.dest, lang)
-            FileUtils.mkdir_p(dir)
+            FileUtils.mkdir_p(File.join(site.dest, lang))
           end
         end
-        # Appel à la génération d’origine (qui utilisera mkdir_p si besoin)
-        orig_generate(site)
+        # puis on appelle la génération « normale »
+        super
       end
     end
+
+    # On préfixe Generator avec notre module de patch
+    Generator.prepend(GeneratorPatch)
   end
 end
